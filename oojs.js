875acc8288862fcb132335fc597e4dfd6f78328b
Angie Spears <angie.spears809@gmail.com>
2017-10-31 13:52:18 -0400

    checkpoint javascript retake

--- oojs.js
+++ oojs.js
@@ -5,22 +5,25 @@
 // - a `members` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
-
-
-
-
+class Group {
+	constructor(name) {
+		this.name = name
+		this.members = []
+	}
+	addMember(person) {
+		this.members.push(person)
+	}
+}
 
 // #2: Create an instance of the Group class and set it to a variable called `classmates`
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
-
-
-
-
+var classmates = new Group('name')
+classmates.addMember('person')
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Group !== 'undefined') {
-  module.exports = {
-    Group
-  }
+if (typeof Group !== 'undefined') {
+	module.exports = {
+		Group
+	}
 }
